a7b5629c81711c8117c2f4a6ac9352a48b8b1ba3
Shagufta Naz <heer@192.168.0.106>
2017-04-22 14:03:04 +0530

    initial

---
+++ productserive/src/main/java/com/heer/ecom/ProductseriveApplication.java
@@ -0,0 +1,14 @@
+package com.heer.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductseriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductseriveApplication.class, args);
+	}
+	
+}
+
